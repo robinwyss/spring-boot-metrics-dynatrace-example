4624b70fc8f33669956a644ce753616edcc2f882
Robin Wyss <rowyss@gmail.com>
2021-03-16 16:37:20 +0100

    Remove unused code

--- src/main/java/com/example/demo/App.java
+++ src/main/java/com/example/demo/App.java
@@ -12,27 +12,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class App {
 	public static void main(String[] args) {
-//		StatsdConfig config = new StatsdConfig() {
-//			@Override
-//			public String get(String k) { return null; }
-//			@Override
-//			public StatsdFlavor flavor() { return StatsdFlavor.ETSY; }
-//
-//			@Override
-//			public int port() {
-//				return 8129;
-//			}
-//
-//			@Override
-//			public boolean enabled() {
-//				return true;
-//			}
-//		};
-//
-//		MeterRegistry registry = new StatsdMeterRegistry(config, Clock.SYSTEM);
-//
-//		Metrics.globalRegistry.add(registry);
-
 		SpringApplication.run(App.class, args);
 	}
 
